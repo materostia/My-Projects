d60046ab9f7752a2efec213039f156c308563de7
materostia <materostia@gmail.com>
2024-08-10 02:56:13 -0800

    Add files via upload

---
+++ PRACTICE/py.py
@@ -0,0 +1,18 @@
+#reverse anything
+
+#name = "Mate Rostiashvili" [::-1]
+#print(name)
+
+#name1 = "Xatia Balashvili" [::-1]
+#print(name1)
+
+#name2 = "Lazare Rostiashvili" [::-1]
+#print(name2)
+
+#name3 = "Giorgi Rostiashvili" [::-1]
+#print(name3)
+
+
+
+for number in range(10):
+    print("hello world !")
